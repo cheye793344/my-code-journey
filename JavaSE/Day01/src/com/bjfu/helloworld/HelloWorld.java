9df176363f788329ee45058fc9f9bdfa52aeba2d
cheye793344 <2965165585@qq.com>
2025-05-29 21:37:01 +0800

    5.29

--- JavaSE/Day01/src/com/bjfu/helloworld/HelloWorld.java
+++ JavaSE/Day01/src/com/bjfu/helloworld/HelloWorld.java
@@ -1,7 +1,30 @@
 package com.bjfu.helloworld;
 
 public class HelloWorld {
+    /**
+     * 程序的主入口点
+     * @param args 命令行参数，未在此程序中使用
+     */
+
     public static void main(String[] args) {
+        // 打印Hello World到控制台
         System.out.println("Hello World");
+            System.out.println(getRandomCode());
+        }
+        //定义一个方法帮我生成一个验证码返回
+    public static String getRandomCode(){
+        String str="ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
+        StringBuilder sb=new StringBuilder();
+        for(int i=0;i<4;i++){
+            int index=(int)(Math.random()*str.length());
+            char ch=str.charAt(index);
+            sb.append(ch);
+        }
+        return sb.toString();
     }
 }
+
+
+
+
+
