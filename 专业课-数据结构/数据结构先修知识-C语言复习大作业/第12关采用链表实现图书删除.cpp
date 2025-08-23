7eb15f125b6ebad2a32e5843501bc3bf7c98d40e
cheye793344 <2965165585@qq.com>
2025-08-23 10:35:37 +0800

    数据结构练习-基于链表的图书信息增删改查-删

---
+++ 专业课-数据结构/数据结构先修知识-C语言复习大作业/第12关采用链表实现图书删除.cpp
@@ -0,0 +1,80 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+typedef struct {
+    char no[20];					//书号 
+    char name[100];					//书名 
+    float price;					//价格 
+}Book;
+
+typedef struct LNode {
+    Book data;                      //图书信息
+    struct LNode* next;             //指向下一结点
+}LNode, * LinkList;
+
+void InitList(LinkList& L) {
+    //使用动态内存分配new进行初始化 
+    L = new LNode;
+    L->next = nullptr;
+}
+
+void FreeList(LinkList& L) {
+    //释放内存
+    LNode* p = L->next;
+    while (p != nullptr) {
+        LNode* temp;
+        temp = p;
+        p = p->next;
+        delete temp;
+    }
+    delete L;
+    L = nullptr;
+}
+
+void ReadFile(LinkList& L, string filepath) {
+    //从文件中读取图书信息，利用尾插法构建链表
+    ifstream infile(filepath.c_str());
+    string line;
+    getline(infile, line);
+    getline(infile, line);
+    Book temp;
+    LNode* tail = L;
+    while (infile >> temp.no >> temp.name >> temp.price) {
+        LNode* newNode = new LNode;
+        newNode->data = temp;
+        newNode->next = nullptr;
+        tail->next = newNode;
+        tail = newNode;
+    }
+    infile.close();
+}
+
+void SaveFile(LinkList& L, string filepath) {
+    //保存图书信息到文件
+    ofstream outfile(filepath.c_str());
+    outfile << "北京林业大学图书馆计算机类图书采购列表" << endl;
+    outfile << "ISBN	                  书名	                定价" << endl;
+    LNode* p = L->next;
+    while (p != nullptr) {
+        outfile << p->data.no << " " << p->data.name << " " << p->data.price << endl;
+        p = p->next;
+    }
+    outfile.close();
+}
+
+bool DeleteBook(LinkList& L, int pos) {
+    //删除指定图书信息
+    //如果删除成功，返回true，否则，返回false
+    if (pos < 1) return false;
+    int currentPos = 0;
+    LNode* prev = L;
+    while (prev != nullptr && currentPos < pos - 1) {
+        prev = prev->next;
+        currentPos++;
+    }
+    if (prev == nullptr || prev->next == nullptr) return false;
+    LNode* temp = prev->next;
+    prev->next = temp->next;
+    if (temp != nullptr) delete temp;
+    return true;
+}
