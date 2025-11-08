e314b2fe7373ed629b6e2bab29ee1fa72a617e47
cheye793344 <2965165585@qq.com>
2025-11-08 21:37:25 +0800

    中缀表达式转化为后缀表达式（补充了关键函数）

--- 专业课-数据结构/编程题实训/实验2-基于栈的算术表达式求值算法/第2关：中缀表达式转化为后缀表达式.cpp
+++ 专业课-数据结构/编程题实训/实验2-基于栈的算术表达式求值算法/第2关：中缀表达式转化为后缀表达式.cpp
@@ -4,56 +4,96 @@ using namespace std;
 #define OK 1
 #define ERROR 0
 #define OVERFLOW -2
+typedef int Status;
 typedef struct
 {
-	char* base;
-	char* top;
-	int stacksize;
+    char* base;
+    char* top;
+    int stacksize;
 }SqStack;
-int InitStack(SqStack& S)
+Status InitStack(SqStack& S)
 {//初始化栈
-	S.base = new char[MAXSIZE];
-	if (!S.base) return ERROR;
-	S.top = S.base;
-	S.stacksize = MAXSIZE;
-	return OK;
+    S.base = new char[MAXSIZE];
+    if (!S.base) return ERROR;
+    S.top = S.base;
+    S.stacksize = MAXSIZE;
+    return OK;
 }
-int Push(SqStack& S, char e)
+Status Push(SqStack& S, char e)
 {//入栈
-	if (S.top - S.base == S.stacksize) return ERROR;
-	*S.top++ = e;
-	return OK;
+    if (S.top - S.base == S.stacksize) return ERROR;
+    *S.top++ = e;
+    return OK;
 }
-int Pop(SqStack& S)
+Status Pop(SqStack& S)
 {//出栈
-	if (S.top == S.base) return ERROR;
-	S.top--;
-	return OK;
+    if (S.top == S.base) return ERROR;
+    S.top--;
+    return OK;
 }
 char GetTop(SqStack S)
 {//取栈顶元素
-	if (S.top == S.base) return ERROR;
-	return *(S.top - 1);
+    if (S.top == S.base) return ERROR;
+    return *(S.top - 1);
 }
 char Precede(char a, char b)
-{//比较符号优先级
-	if (a == '+' || a == '-') {
-		if (b == '+' || b == '-' || b == ')' || b == '=') return '>';
-		else return '<';
-	}
-	else if (a == '*' || a == '/') {
-		if (b == '(') return '<';
-		else return '>';
-	}
-	else if (a == '(') {
-		if (b == ')') return '=';
-		else return '<';
-	}
-	else if (a == ')') {
-		return '>';
-	}
-	else if (a == '=') {
-		if (b == '=') return '=';
-		else return '<';
-	}
+{//比较运算符a和b的优先级
+    if (a == '+' || a == '-') {
+        if (b == '+' || b == '-' || b == ')' || b == '=') return '>';
+        else return '<';
+    }
+    else if (a == '*' || a == '/') {
+        if (b == '(') return '<';
+        else return '>';
+    }
+    else if (a == '(') {
+        if (b == ')') return '=';
+        else return '<';
+    }
+    else if (a == ')') {
+        return '>';
+    }
+    else if (a == '=') {
+        if (b == '=') return '=';
+        else return '<';
+    }
+}
+void InfixToSuffix(SqStack OPTR, char s[])
+{//将中缀表达式转化为后缀表达式并输出 
+    InitStack(OPTR);
+    Push(OPTR, '=');
+
+    int i = 0;
+    char ch = s[i];
+    while (ch != '=' || GetTop(OPTR) != '=') {
+        if (ch >= '0' && ch <= '9') {
+            cout << ch;
+            ch = s[++i];
+        }
+        else {
+            char op;
+            switch (Precede(GetTop(OPTR), ch)) {
+            case '>':
+                op = GetTop(OPTR);
+                Pop(OPTR);
+                cout << op;
+                break;
+
+            case '<':
+                Push(OPTR, ch);
+                ch = s[++i];
+                break;
+
+            case '=':
+                Pop(OPTR);
+                ch = s[++i];
+                break;
+            }
+        }
+    }
+    while (GetTop(OPTR) != '=') {
+        cout << GetTop(OPTR);
+        Pop(OPTR);
+    }
+    if (GetTop(OPTR) == '=') cout << endl;
 }
