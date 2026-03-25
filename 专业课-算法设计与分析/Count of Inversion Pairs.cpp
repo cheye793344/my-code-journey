c0ed734f93c6438948bc55b8ab08f0b0dc165cfa
cheye793344 <2965165585@qq.com>
2026-03-25 23:02:52 +0800

    Count of Inversion Pairs

---
+++ 专业课-算法设计与分析/Count of Inversion Pairs.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+int Counter() {
+	int n;
+	cin >> n; 
+    vector<int> a(n);
+    for (int i = 0; i < n; i++) {
+        cin >> a[i];
+    }
+    int count = 0;
+    for (int i = 0; i < n; i++) {
+        for (int j = i + 1; j < n; j++) {
+            if (a[i] > a[j]) {
+                count++;
+            }
+        }
+    }
+    cout << count;
+}
+
+int main() {
+    Counter();
+    return 0;
+}
+
